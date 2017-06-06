9706f233f8b9a5ce70818d0a55e8f16bdfbd1701
oONinaOo <loveninagold@gmail.com>
2017-06-06 07:24:50 +0200

    finished CreateClass

---
+++ CreateClass/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Employee : Person, ICloneable
+    {
+        public Room Room;
+
+        private int salary;
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        private string profession;
+
+        public string Profession
+        {
+            get { return profession; }
+            set { profession = value; }
+        }
+        public Employee()
+        {
+            Room = null;
+        }
+
+        public Employee(string name, DateTime birthDate, int salary, string profession) : base(name, birthDate)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            Room = null;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + String.Format(", salary: {0},  profession: {1}, room: {2}", salary, profession, Room.Number);
+        }
+
+        public object Clone()
+        {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.Room = new Room(Room.Number);
+            return newEmployee;
+        }
+    }
+}
---
+++ CreateClass/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private DateTime birthDate;
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+        public Genders gender;
+        public Person(){}
+        public Person(String Name, DateTime birthDate)
+        {
+            this.Name = Name;
+            this.birthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return (String.Format("name: {0}, birth date: {1}", this.name, this.birthDate));
+        }
+    }
+}
